e9956b8a3ff2f71b10c66bfe659accb69be9ba32
Alex Rothenberg <alexroth96@gmail.com>
2020-03-31 18:06:07 -0400

    redesign header

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import navStyle from './Nav.module.scss';
 
-<<<<<<< HEAD
 const Nav = (props: any) => {
 	return (
 		<nav className={navStyle.nav}>
@@ -11,22 +10,6 @@ const Nav = (props: any) => {
 			</div>
 		</nav>
 	);
-=======
-const Nav = () => {
-  return <nav className={navStyle.nav}>
-    <div className={navStyle.nav__inner}>
-      <div className={navStyle.nav__left}>
-        <a>SATACTSENSE</a>
-      </div>
-      <div className={navStyle.nav__right}>
-        <a>Test Preparation</a>
-        <a>Success Stories</a>
-        <a>About Us</a>
-        <a>Contact</a>
-      </div>
-    </div>
-  </nav>;
->>>>>>> 2a3aa5ad0f60e7c98b1d36c9f91b172fe18d9d98
 };
 
 Nav.propTypes = {};
