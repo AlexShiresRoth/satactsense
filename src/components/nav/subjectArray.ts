81fc264c510afb1804ca8eed829b0069785476f7
Alex Roth <alexroth96@gmail.com>
2020-04-19 13:15:45 -0400

    add general ta

--- src/components/nav/subjectArray.ts
+++ src/components/nav/subjectArray.ts
@@ -1,103 +1,139 @@
 //White tabs under the blue
 export const subjectArray = [
-	{
-		id: 'AP',
-		types: [
-			{ title: 'AP Math', dropdown: ['AP Calculus & Statistics'] },
-			{ title: 'AP History', dropdown: ['AP US History', 'AP World History'] },
-			{ title: 'AP English', dropdown: ['AP Language', 'AP Reading', 'AP Writing'] },
-			{
-				title: 'AP Science',
-				dropdown: ['AP Physics', 'AP Chemistry', 'AP Living Environment', 'AP Biology'],
-			},
-			{ title: 'AP Business', dropdown: ['AP Micro or Macro Economics'] },
-			{ title: 'AP Foreign Language', dropdown: ['AP Spanish'] },
-			{ title: 'AP Psychology', dropdown: ['AP Psychology'] },
-		],
-	},
-	{
-		id: 'SUBJECT',
-		types: [
-			{ title: 'Literature', dropdown: ['Literature (SAT II Subject Test)'] },
-			{ title: 'History', dropdown: ['World History (SAT II Subject Test)', 'US History (SAT II Subject Test)'] },
-			{ title: 'Mathematics', dropdown: ['Level 1 (SAT II Subject Test)', 'Level 2 (SAT II Subject Test)'] },
-			{
-				title: 'Sciences',
-				dropdown: [
-					'Biology: (Ecological and Molecular) (SAT II Subject Test)',
-					'Physics (SAT II Subject Test)',
-					'Chemistry (SAT II Subject Test)',
-				],
-			},
-			{ title: 'Languages', dropdown: ['Spanish (SAT II Subject Test)', 'French (SAT II Subject Test)'] },
-		],
-	},
-	{
-		id: 'PSAT/PLAN',
-		types: [
-			{ title: 'PSAT 10th Grade', dropdown: ['PSAT 10th Grade'] },
-			{ title: 'PSAT 11th Grade', dropdown: ['PSAT 11th Grade'] },
-			{ title: 'PLAN 10th Grade', dropdown: ['PLAN 10th Grade'] },
-			{ title: 'PLAN 11th Grade', dropdown: ['PLAN 11th Grade'] },
-		],
-	},
-	{
-		id: 'SAT/ACT',
-		types: [{ title: '', dropdown: [] }],
-	},
-	{
-		id: 'GENERAL',
-		types: [{ title: '', dropdown: [] }],
-	},
-	{
-		id: 'CTY',
-		types: [
-			{ title: 'SCAT (Grades 2-8)', dropdown: ['SCAT'] },
-			{ title: 'PSAT & SCAT (Grades 5-6)', dropdown: ['PSAT & SCAT'] },
-			{ title: 'SAT & SCAT (Grades 7-8)', dropdown: ['SAT & SCAT'] },
-		],
-	},
+  {
+    id: "AP",
+    types: [
+      { title: "AP Math", dropdown: ["AP Calculus & Statistics"] },
+      { title: "AP History", dropdown: ["AP US History", "AP World History"] },
+      {
+        title: "AP English",
+        dropdown: ["AP Language", "AP Reading", "AP Writing"],
+      },
+      {
+        title: "AP Science",
+        dropdown: [
+          "AP Physics",
+          "AP Chemistry",
+          "AP Living Environment",
+          "AP Biology",
+        ],
+      },
+      { title: "AP Business", dropdown: ["AP Micro or Macro Economics"] },
+      { title: "AP Foreign Language", dropdown: ["AP Spanish"] },
+      { title: "AP Psychology", dropdown: ["AP Psychology"] },
+    ],
+  },
+  {
+    id: "SUBJECT",
+    types: [
+      { title: "Literature", dropdown: ["Literature (SAT II Subject Test)"] },
+      {
+        title: "History",
+        dropdown: [
+          "World History (SAT II Subject Test)",
+          "US History (SAT II Subject Test)",
+        ],
+      },
+      {
+        title: "Mathematics",
+        dropdown: [
+          "Level 1 (SAT II Subject Test)",
+          "Level 2 (SAT II Subject Test)",
+        ],
+      },
+      {
+        title: "Sciences",
+        dropdown: [
+          "Biology: (Ecological and Molecular) (SAT II Subject Test)",
+          "Physics (SAT II Subject Test)",
+          "Chemistry (SAT II Subject Test)",
+        ],
+      },
+      {
+        title: "Languages",
+        dropdown: [
+          "Spanish (SAT II Subject Test)",
+          "French (SAT II Subject Test)",
+        ],
+      },
+    ],
+  },
+  {
+    id: "PSAT/PLAN",
+    types: [
+      { title: "PSAT 10th Grade", dropdown: ["PSAT 10th Grade"] },
+      { title: "PSAT 11th Grade", dropdown: ["PSAT 11th Grade"] },
+      { title: "PLAN 10th Grade", dropdown: ["PLAN 10th Grade"] },
+      { title: "PLAN 11th Grade", dropdown: ["PLAN 11th Grade"] },
+    ],
+  },
+  {
+    id: "SAT/ACT",
+    types: [{ title: "", dropdown: [] }],
+  },
+  {
+    id: "GENERAL",
+    types: [
+      { title: "Mathematics", dropdown: ["Mathematics"] },
+      { title: "History", dropdown: ["History"] },
+	  { title: "Sciences", dropdown: ["Sciences"] },
+	  { title: 'Languages', dropdown: ['Languages'] },
+	  { title: 'Business Courses', dropdown: ['Business Courses'] },
+	  { title: 'College Courses', dropdown: ['College Courses'] },
+	  { title: 'Elementary & Middle School', dropdown: ['Elementatry & Middle School'] }
+    ],
+  },
+  {
+    id: "CTY",
+    types: [
+      { title: "SCAT (Grades 2-8)", dropdown: ["SCAT"] },
+      { title: "PSAT & SCAT (Grades 5-6)", dropdown: ["PSAT & SCAT"] },
+      { title: "SAT & SCAT (Grades 7-8)", dropdown: ["SAT & SCAT"] },
+    ],
+  },
 ];
 
 //First set of tabs(Blue)
 export const serviceLinks = [
-	{
-		title: 'AP/SAT II Subject Testing',
-		path: '/ap',
-		id: 'AP',
-		dropdown: [
-			{ title: 'AP Testing', dropid: 'AP' },
-			{ title: 'SAT II Subject Testing', dropid: 'SUBJECT' },
-		],
-	},
-	{
-		title: 'PSAT/PLAN',
-		path: '/psat',
-		id: 'PSAT/PLAN',
-		dropdown: [
-			{ title: 'PSAT', dropid: 'PSAT/PLAN' },
-			{ title: 'PLAN', dropid: 'PSAT/PLAN' },
-		],
-	},
-	{
-		title: 'SAT/ACT Prep',
-		path: '/satact',
-		id: 'SAT/ACT',
-		dropdown: [
-			{ title: 'SAT', dropid: 'SAT' },
-			{ title: 'ACT', dropid: 'ACT' },
-		],
-	},
-	{
-		title: 'General Tutoring',
-		path: '/general',
-		id: 'GENERAL',
-		dropdown: [],
-	},
-	{
-		title: 'Johns Hopkins Program for Advanced Testing',
-		path: '/jht',
-		id: 'CTY',
-		dropdown: [{ title: 'CTY Program-Center for Talented Youth', dropid: 'CTY' }],
-	},
+  {
+    title: "AP/SAT II Subject Testing",
+    path: "/ap",
+    id: "AP",
+    dropdown: [
+      { title: "AP Testing", dropid: "AP" },
+      { title: "SAT II Subject Testing", dropid: "SUBJECT" },
+    ],
+  },
+  {
+    title: "PSAT/PLAN",
+    path: "/psat",
+    id: "PSAT/PLAN",
+    dropdown: [
+      { title: "PSAT", dropid: "PSAT/PLAN" },
+      { title: "PLAN", dropid: "PSAT/PLAN" },
+    ],
+  },
+  {
+    title: "SAT/ACT Prep",
+    path: "/satact",
+    id: "SAT/ACT",
+    dropdown: [
+      { title: "SAT", dropid: "SAT" },
+      { title: "ACT", dropid: "ACT" },
+    ],
+  },
+  {
+    title: "General Tutoring",
+    path: "/general",
+    id: "GENERAL",
+    dropdown: [],
+  },
+  {
+    title: "Johns Hopkins Program for Advanced Testing",
+    path: "/jht",
+    id: "CTY",
+    dropdown: [
+      { title: "CTY Program-Center for Talented Youth", dropid: "CTY" },
+    ],
+  },
 ];
