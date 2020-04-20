686fdca471d5d0583e5beeb90a898514f6addc51
Alex Rothenberg <alexroth96@gmail.com>
2020-04-20 13:19:49 -0400

    allow merge

--- src/components/home/headerText.ts
+++ src/components/home/headerText.ts
@@ -1,85 +1,81 @@
 export const headerText = [
-  {
-    title: "AP Testing",
-    id: "AP",
-    banner: "Are you ready for the new VIRTUAL AP exam in May 2020?",
-    text: [
-      `Is the new structure and format confusing?`,
-      `Our experienced AP tutors/instructors can help navigate you through the material and the online
+	{
+		title: 'AP Testing',
+		id: 'AP',
+		banner: 'Are you ready for the new VIRTUAL AP exam in May 2020?',
+		text: [
+			`Is the new structure and format confusing?`,
+			`Our experienced AP tutors/instructors can help navigate you through the material and the online
 						format of the AP exam.`,
-      `We can offer remote tutoring for a variety of the exams based on the student's availability,
+			`We can offer remote tutoring for a variety of the exams based on the student's availability,
 						scheduling, and skillset.`,
-      `Click the ones that apply.`,
-    ],
-  },
-  {
-    title: "SAT II Subject Testing",
-    id: "SUBJECT",
-    banner:
-      "Some colleges require SAT II Subject Testing in addition to regular SAT tests.",
-    text: [
-      `All of our prospective students take a qualifying test to see if the student scores at a certain range to make preparing for the test worth it.`,
+			`Click the ones that apply.`,
+		],
+	},
+	{
+		title: 'SAT II Subject Testing',
+		id: 'SUBJECT',
+		banner: 'Some colleges require SAT II Subject Testing in addition to regular SAT tests.',
+		text: [
+			`All of our prospective students take a qualifying test to see if the student scores at a certain range to make preparing for the test worth it.`,
 
-      `As a student/parent, how do I know if my son/daughter should take an SAT II Subject Test?
+			`As a student/parent, how do I know if my son/daughter should take an SAT II Subject Test?
 `,
-      `Students generally take the SAT II Subject Test in June following AP exam in May.`,
-    ],
-  },
-  {
-    title:
-      "Johns Hopkins Program For Advanced Testing: The Center for Talented Youth.",
-    id: "CTY",
-    banner: `For the Johns Hopkins program, which test a student can take depends on their grade level, as listed above.`,
-    text: [
-      `The school and ability tests (SCAT test) is offered only to students between grades 2-8.`,
-      `The SCAT test contains a quantiative and verbal assessment.`,
-      `The SCAT test is computer based, timed, multiple choice and offers flexible scheduling.`,
-      `Please contact us for more information.`,
-    ],
-  },
-  {
-    title: "The PSAT & PLAN (Shortened version of the ACT)",
-    id: "PSAT/PLAN",
-    banner: ``,
-    text: [
-      `Both tests are typically taken in 10th and 11th grades as a precursor to the SAT/ACT.`,
-      `These tests are typically oferred once a year, and are conducted in the school on an assigned date.`,
-      `Taken as a 10th or 11th grader, the PSAT qualifies students for a National Merit Scholarship.
+			`Students generally take the SAT II Subject Test in June following AP exam in May.`,
+		],
+	},
+	{
+		title: 'Johns Hopkins Program For Advanced Testing: The Center for Talented Youth.',
+		id: 'CTY',
+		banner: `For the Johns Hopkins program, which test a student can take depends on their grade level, as listed above.`,
+		text: [
+			`The school and ability tests (SCAT test) is offered only to students between grades 2-8.`,
+			`The SCAT test contains a quantiative and verbal assessment.`,
+			`The SCAT test is computer based, timed, multiple choice and offers flexible scheduling.`,
+			`Please contact us for more information.`,
+		],
+	},
+	{
+		title: 'The PSAT & PLAN (Shortened version of the ACT)',
+		id: 'PSAT/PLAN',
+		banner: ``,
+		text: [
+			`Both tests are typically taken in 10th and 11th grades as a precursor to the SAT/ACT.`,
+			`These tests are typically oferred once a year, and are conducted in the school on an assigned date.`,
+			`Taken as a 10th or 11th grader, the PSAT qualifies students for a National Merit Scholarship.
 			 All questions are multiple choice (no essay).`,
-      `Typically students prepare for these tests in the mid summer going into 10th/11th grade.`,
-    ],
-  },
-  {
-    title: "General Tutoring Sessions",
-    id: "GENERAL",
-    banner:
-      "We offer high quality online tutors for any academic/school subject.",
-    text: [
-      "Experienced and certified teachers from K-12.",
-	  `We can tutor for any high school and college course subject from Anthropology to Zoology.`,
-	  `Elementary and Middle School tutoring also available (Reading/Writing/Math skills).`,
-	  `1 Hour lessons (Typically).`,
-	  `Flexible Scheduling (On your son/daughter's schedule not the tutors).`,
-	  `Tutors typically communicate with students PRIOR to the lesson so they know what material to cover. `,
-	  `No Commitment other than one lesson at a time. `
-    ],
-  },
-  {
-    title: "SAT/ACT",
-    id: "SAT/ACT",
-    banner: "",
-    text: [],
-  },
-  {
-    title: "SAT Preperation",
-    id: "SAT",
-    banner: "",
-    text: [],
-  },
-  {
-    title: "ACT Preparation",
-    id: "ACT",
-    banner: "",
-    text: [],
-  },
+			`Typically students prepare for these tests in the mid summer going into 10th/11th grade.`,
+		],
+	},
+	{
+		title: 'General Tutoring Sessions',
+		id: 'GENERAL',
+		banner: `We can tutor for any high school and college course subject from Anthropology to Zoology.`,
+		text: [
+			'Experienced and certified teachers from K-12.',
+			`Elementary and Middle School tutoring also available (Reading/Writing/Math skills).`,
+			`1 Hour lessons (Typically).`,
+			`Flexible Scheduling (On your son/daughter's schedule not the tutors).`,
+			`Tutors typically communicate with students PRIOR to the lesson so they know what material to cover. `,
+			`No Commitment other than one lesson at a time. `,
+		],
+	},
+	{
+		title: 'SAT/ACT',
+		id: 'SAT/ACT',
+		banner: '',
+		text: [],
+	},
+	{
+		title: 'SAT Preperation',
+		id: 'SAT',
+		banner: '',
+		text: [],
+	},
+	{
+		title: 'ACT Preparation',
+		id: 'ACT',
+		banner: '',
+		text: [],
+	},
 ];
