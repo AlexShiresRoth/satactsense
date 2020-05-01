38415e968b044a2ac4f2ebbdf4ab7ac47408fd17
Alex Rothenberg <alexroth96@gmail.com>
2020-04-30 21:38:22 -0400

    add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import ReactGA from 'react-ga';
 import store from './store';
 import './css/main.css';
+
+ReactGA.initialize('UA-165289420-1');
+ReactGA.pageview('/');
 
 const App: React.FunctionComponent = () => {
 	return (
