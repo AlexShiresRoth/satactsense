673b92b6023e7331f2b0b00e96d722846eee3460
Alex Rothenberg <alexroth96@gmail.com>
2021-07-24 13:40:26 -0400

    fix links

--- src/components/actmaterials/materials.ts
+++ src/components/actmaterials/materials.ts
@@ -1,44 +1,47 @@
 export const materials = [
-	{
-		title: `Princeton Review ACT Premium Prep, 2020`,
-		url: `https://www.amazon.com/gp/product/0525568808/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0525568808&linkCode=as2&tag=satactsense0d-20&linkId=2f9799713eb186388dc36f401e89795e#ace-8881249860`,
-		img: `https://res.cloudinary.com/snackmanproductions/image/upload/v1591313258/satactsense/actpremiumprep_ccoxsr.jpg`,
-	},
-	{
-		title: `ACT Practice Questions`,
-		url: `https://www.amazon.com/gp/product/0525567909/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0525567909&linkCode=as2&tag=satactsense0d-20&linkId=dc5bd4d832ce624cf8f35b95101816d7`,
-		img: `https://res.cloudinary.com/snackmanproductions/image/upload/v1591313391/satactsense/actpracticequestions_mvyxcq.jpg`,
-	},
-	{
-		title: `Barron's ACT Flash Cards`,
-		url: `https://www.amazon.com/gp/product/0764167685/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0764167685&linkCode=as2&tag=satactsense0d-20&linkId=f691b283fad083455d4a9366c83fa89b`,
-		img: `https://res.cloudinary.com/snackmanproductions/image/upload/v1591313450/satactsense/51aq9mCnwiL._SY438_BO1_204_203_200__g7pvcw.jpg`,
-	},
-	{
-		title: `ACT Pacing Digital Timer and Watch`,
-		url: `https://www.amazon.com/gp/product/B01C77MV0U/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B01C77MV0U&linkCode=as2&tag=satactsense0d-20&linkId=c23bbe3c5caff9338a79cbd54cccdff8`,
-		img: `https://res.cloudinary.com/snackmanproductions/image/upload/v1591313511/satactsense/81ii4YFktyL._SL1500__tbqhzf.jpg`,
-	},
-	{
-		title: `5 - Subject Notebook`,
-		url: `https://www.amazon.com/gp/product/B00X7X2KIC/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00X7X2KIC&linkCode=as2&tag=satactsense0d-20&linkId=ca3c2e22f4fdbff50f232ffdec4eecc9`,
-		img: `https://res.cloudinary.com/snackmanproductions/image/upload/v1591313572/satactsense/5196IdZAFGL._AC_SL1001__wbkoek.jpg`,
-	},
-	{
-		title: `Sharpened Pencils`,
-		url: `https://www.amazon.com/gp/product/B071JM699B/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B071JM699B&linkCode=as2&tag=satactsense0d-20&linkId=90c2f0a0e271564b1c5a53c37e25a8a0`,
-		img: `https://res.cloudinary.com/snackmanproductions/image/upload/v1591313648/satactsense/51jJpU8q6kL._AC_SL1434__a2c5wh.jpg`,
-	},
-	{
-		title: `iOS: OutRead`,
-		url: `https://apps.apple.com/us/app/outread-speed-reading-trainer/id778846279`,
-		img: `https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_61,w_443/v1591314079/satactsense/2020-06-04_h4nuhu.png`,
-	},
-	{
-		title: `Android: Speed Reading`,
-		url: `https://play.google.com/store/apps/details?id=com.speedreading.alexander.speedreading`,
-		img:
-			'https://res.cloudinary.com/snackmanproductions/image/upload/v1591314448/satactsense/android-speed-reading-app-trainer_q2mcai.jpg',
-	},
-	{ title: `ACT English Manual(Copyright Steven Rossner) $125(e - mailed as PDF attachment)`, url: ``, img: '' },
+  {
+    title: `Princeton Review ACT Premium Prep, 2022`,
+    url: `https://amzn.to/3kLQJwH`,
+    img: `https://images-na.ssl-images-amazon.com/images/I/51siOqfBw8S._SX382_BO1,204,203,200_.jpg`,
+  },
+  {
+    title: `ACT Practice Questions`,
+    url: `https://amzn.to/3zvYevP`,
+    img: `https://images-na.ssl-images-amazon.com/images/I/51r-ZQOxTNL._SX383_BO1,204,203,200_.jpg`,
+  },
+  {
+    title: `Barron's ACT Flash Cards`,
+    url: `https://amzn.to/3rAJKYH`,
+    img: `https://images-na.ssl-images-amazon.com/images/I/41AilkhIWzL._SY419_BO1,204,203,200_.jpg`,
+  },
+  {
+    title: `ACT Pacing Digital Timer and Watch`,
+    url: `https://amzn.to/3y3oVaL`,
+    img: `https://m.media-amazon.com/images/I/817XJiAKwFL._SX522_.jpg`,
+  },
+  {
+    title: `5 - Subject Notebook`,
+    url: `https://www.amazon.com/gp/product/B00X7X2KIC/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00X7X2KIC&linkCode=as2&tag=satactsense0d-20&linkId=ca3c2e22f4fdbff50f232ffdec4eecc9`,
+    img: `https://res.cloudinary.com/snackmanproductions/image/upload/v1591313572/satactsense/5196IdZAFGL._AC_SL1001__wbkoek.jpg`,
+  },
+  {
+    title: `Sharpened Pencils`,
+    url: `https://www.amazon.com/gp/product/B071JM699B/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B071JM699B&linkCode=as2&tag=satactsense0d-20&linkId=90c2f0a0e271564b1c5a53c37e25a8a0`,
+    img: `https://res.cloudinary.com/snackmanproductions/image/upload/v1591313648/satactsense/51jJpU8q6kL._AC_SL1434__a2c5wh.jpg`,
+  },
+  {
+    title: `iOS: OutRead`,
+    url: `https://apps.apple.com/us/app/outread-speed-reading-trainer/id778846279`,
+    img: `https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_61,w_443/v1591314079/satactsense/2020-06-04_h4nuhu.png`,
+  },
+  {
+    title: `Android: Speed Reading`,
+    url: `https://play.google.com/store/apps/details?id=com.speedreading.alexander.speedreading`,
+    img: "https://res.cloudinary.com/snackmanproductions/image/upload/v1591314448/satactsense/android-speed-reading-app-trainer_q2mcai.jpg",
+  },
+  {
+    title: `ACT English Manual(Copyright Steven Rossner) $125(e - mailed as PDF attachment)`,
+    url: ``,
+    img: "",
+  },
 ];
