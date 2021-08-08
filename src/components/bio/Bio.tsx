51e2e154666de36234152c56fc7df1236789000f
Alex Rothenberg <alexroth96@gmail.com>
2021-08-08 14:30:36 -0400

    add testimonial

--- src/components/bio/Bio.tsx
+++ src/components/bio/Bio.tsx
@@ -7,26 +7,28 @@ const Bio = () => {
 
   return (
     <section className={bioStyle.section}>
-      <div className={bioStyle.img_container}>
-        <img
-          src={`https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_63,w_708/v1588264175/satactsense/steve_uav2de.jpg`}
-          alt="Steve Rossner"
-        />
-        <p>- Steve Rossner, Founder of SAT/ACT Sense</p>
-      </div>
-      <div className={bioStyle.container}>
-        <h2>About SAT/ACT Sense</h2>
-        <div className={reduced ? bioStyle.copy_reduced : bioStyle.copy}>
-          {content.slice(0, reduced ? 2 : content.length).map((item, i) => {
-            return <div key={i}>{item.par}</div>;
-          })}
+      <div className={bioStyle.inner}>
+        <div className={bioStyle.img_container}>
+          <img
+            src={`https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_63,w_708/v1588264175/satactsense/steve_uav2de.jpg`}
+            alt="Steve Rossner"
+          />
+          <p>- Steve Rossner, Founder of SAT/ACT Sense</p>
         </div>
-        <div className={bioStyle.copy_button_container}>
-          <span></span>
-          <button onClick={() => setSize(!reduced)}>
-            {reduced ? "Read More" : "Show Less"}
-          </button>
-          <span></span>
+        <div className={bioStyle.container}>
+          <h2>About SAT/ACT Sense</h2>
+          <div className={reduced ? bioStyle.copy_reduced : bioStyle.copy}>
+            {content.slice(0, reduced ? 2 : content.length).map((item, i) => {
+              return <div key={i}>{item.par}</div>;
+            })}
+          </div>
+          <div className={bioStyle.copy_button_container}>
+            <span></span>
+            <button onClick={() => setSize(!reduced)}>
+              {reduced ? "Read More" : "Show Less"}
+            </button>
+            <span></span>
+          </div>
         </div>
       </div>
     </section>
--- src/components/testimonials/ratings.tsx
+++ src/components/testimonials/ratings.tsx
@@ -1,25 +1,28 @@
 export const ratings = [
-	{
-		text:
-			'Our family has used SAT/ACT Sense for over five years and we have been extremely satisfied with the service and the overall program. Each of our four children made tremendous progress in his or her respective SAT or ACT scores from the initial diagnostic exercise through the final exam. We have worked with many different tutors over the course of 5 years and four students and have always found the tutors to be knowledgeable, personable, and enthusiastic about their subject and about teaching. We’ve also been impressed with Steve’s judgement around making tactical moves to mix up the tutors  and try a different approach when necessary to “punch up” a student’s score at the end of the road. Overall, we would highly recommend SAT/ACT sense. While it is not the cheapest tutoring service out there, we view it as a premium product in a competitive space—and at the end of the day, you get what you pay for.',
-		author: `J. McDonald, Garden City, NY`,
-	},
-	{
-		text: `SAT/ACT Sense helped me to overcome my test anxieties and time management issues which other tutors and test prep companies could not do. I started out at with a 510 on my critical reading and they helped me bring my score up to 650 in just a few months. Most importantly the tutor helped me understand how to take the test and how to eliminate many wrong answers, which put me in a better position to answer the question correctly, especially in the reading comprehension section. I loved the fact that SAT/ACT Sense had proctors who would come to my home and administer a private SAT Test for me -this really helped me get comfortable with the test.`,
-		author: `AJ S. (Manhasset, NY ) Accepted into TUFTS College`,
-	},
-	{
-		text: `We first enrolled Michael in a big name test prep company at our school but quickly realized that he was not getting any value out of the class. He started off by taking the SAT but his scores were not strong and then SAT/ACT Sense helped him focus on the ACT. He started out with a 20 on his Diagnostic test but brought his composite up to a 25 on the official ACT test. The tutors were very accommodating in regards to scheduling and sometimes would stay an extra hour to review material with my son without charging. The tutors were first class and committed beyond our expectations`,
-		author: `Mother of Michael G. (St. Mary’s HS) Accepted into Marist College`,
-	},
-	{
-		text: `We needed someone who could work around an athlete’s schedule and provide a no-nonsense approach in making sure my daughter would complete all the work before the tutors arrived. SAT/ACT Sense did a great job in not only preparing her for the test but helping her understand the concept of time-management and become a better test taker. She started out with a 1620 and brought her score up to 1900. What I liked most about SAT/ACT Sense is that the tutor would sit down with me after the lesson and basically review with me how my daughter was progressing. The tutor would spend an extra 30 minutes sometimes just explaining to me and my daughter what we needed to do as a family to help her maximize her scores. As a parent, I was extremely impressed with the personalized feedback during and after the lesson.`,
-		author: `Anonymous Soccer Mom, (Scarsdale, NY) Accepted into Davidson College`,
-	},
-	{
-		text: `SAT/ACT Sense helped Gabby crack a 700 across the board, which eventually played an integral role in her being the first child in our family to attend an Ivy League School. Before we even started, the tutor came to our home and provided a free demo lesson to make sure we were comfortable with his teaching style and we established a good rapport right at the onset.
+  {
+    text: "SAT/ACT Sense services are exceptional.  Steve Rossner, the owner of the company, and his tutors raise the game on student SAT/ACT achievement.  Steve and his team had the testing knowledge and subject proficiency to help my daughter reach and exceed her goals.  Not only does their philosophy work for college board exams, but also their tutors are the best I’ve ever used for AP test prep as well. I was continuously  impressed. As a mother of three, I would not hesitate to use Steve’s services for all of my children.",
+    author: " Rachel H from Manhasset",
+  },
+  {
+    text: "Our family has used SAT/ACT Sense for over five years and we have been extremely satisfied with the service and the overall program. Each of our four children made tremendous progress in his or her respective SAT or ACT scores from the initial diagnostic exercise through the final exam. We have worked with many different tutors over the course of 5 years and four students and have always found the tutors to be knowledgeable, personable, and enthusiastic about their subject and about teaching. We’ve also been impressed with Steve’s judgement around making tactical moves to mix up the tutors  and try a different approach when necessary to “punch up” a student’s score at the end of the road. Overall, we would highly recommend SAT/ACT sense. While it is not the cheapest tutoring service out there, we view it as a premium product in a competitive space—and at the end of the day, you get what you pay for.",
+    author: `J. McDonald, Garden City, NY`,
+  },
+  {
+    text: `SAT/ACT Sense helped me to overcome my test anxieties and time management issues which other tutors and test prep companies could not do. I started out at with a 510 on my critical reading and they helped me bring my score up to 650 in just a few months. Most importantly the tutor helped me understand how to take the test and how to eliminate many wrong answers, which put me in a better position to answer the question correctly, especially in the reading comprehension section. I loved the fact that SAT/ACT Sense had proctors who would come to my home and administer a private SAT Test for me -this really helped me get comfortable with the test.`,
+    author: `AJ S. (Manhasset, NY ) Accepted into TUFTS College`,
+  },
+  {
+    text: `We first enrolled Michael in a big name test prep company at our school but quickly realized that he was not getting any value out of the class. He started off by taking the SAT but his scores were not strong and then SAT/ACT Sense helped him focus on the ACT. He started out with a 20 on his Diagnostic test but brought his composite up to a 25 on the official ACT test. The tutors were very accommodating in regards to scheduling and sometimes would stay an extra hour to review material with my son without charging. The tutors were first class and committed beyond our expectations`,
+    author: `Mother of Michael G. (St. Mary’s HS) Accepted into Marist College`,
+  },
+  {
+    text: `We needed someone who could work around an athlete’s schedule and provide a no-nonsense approach in making sure my daughter would complete all the work before the tutors arrived. SAT/ACT Sense did a great job in not only preparing her for the test but helping her understand the concept of time-management and become a better test taker. She started out with a 1620 and brought her score up to 1900. What I liked most about SAT/ACT Sense is that the tutor would sit down with me after the lesson and basically review with me how my daughter was progressing. The tutor would spend an extra 30 minutes sometimes just explaining to me and my daughter what we needed to do as a family to help her maximize her scores. As a parent, I was extremely impressed with the personalized feedback during and after the lesson.`,
+    author: `Anonymous Soccer Mom, (Scarsdale, NY) Accepted into Davidson College`,
+  },
+  {
+    text: `SAT/ACT Sense helped Gabby crack a 700 across the board, which eventually played an integral role in her being the first child in our family to attend an Ivy League School. Before we even started, the tutor came to our home and provided a free demo lesson to make sure we were comfortable with his teaching style and we established a good rapport right at the onset.
 
 Each tutor customized a program to help Gabby tackle the specific areas she was weak on. In addition, the tutor also helped my daughter with her college essays. Thanks again for helping Gabby to become the first in our family to get into an Ivy League School`,
-		author: `Mother of Gabby F. (Sachem HS East) Cornell University`,
-	},
+    author: `Mother of Gabby F. (Sachem HS East) Cornell University`,
+  },
 ];
